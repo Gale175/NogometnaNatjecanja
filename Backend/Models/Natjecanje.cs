64fd2bb9389d66bc3adf3aff07b3d9d400f542f4
Martin Galik <gale1508@gmail.com>
2025-02-10 21:06:40 +0100

    Novi model- natjecanja

---
+++ Backend/Models/Natjecanje.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Models
+{
+    public class Natjecanje : Entitet
+    {
+        public int Sifra { get; set; }
+        public string Naziv { get; set; }
+        public string Vrsta { get; set; }
+        public string Drzava { get; set; }
+        public string Sezona { get; set; }
+        public string Pobjednik { get; set; }
+        public string? NajboljiIgrac { get; set; }
+
+
+    }
+}
