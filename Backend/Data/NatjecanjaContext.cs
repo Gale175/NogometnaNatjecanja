dd8423453488e5dbc66919d3fa0ad56f6dfc9e61
Martin Galik <gale1508@gmail.com>
2025-02-12 19:19:29 +0100

    Dodavanje contexta

---
+++ Backend/Data/NatjecanjaContext.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class NatjecanjaContext : DbContext
+    {
+        public NatjecanjaContext(DbContextOptions<NatjecanjaContext> opcije) : base(opcije)
+        {
+            //ovdje se  mogu fino postaviti opcije, ali ne za sada
+        }
+        public DbSet<Natjecanje> Natjecanja { get; set; } // zbog ovog ovdje Natjecanje tablica se zove u mnozini
+    }
+}
