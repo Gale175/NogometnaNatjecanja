09a74dd03e6e612376ae55a5c1022eaf5673e612
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2025-02-28 20:15:54 +0100

    FIX

--- Backend/Models/Natjecanje.cs
+++ Backend/Models/Natjecanje.cs
@@ -4,11 +4,11 @@ namespace Backend.Models
 {
     public class Natjecanje : Entitet
     {
-        public string Naziv { get; set; }
-        public string Vrsta { get; set; }
-        public string Drzava { get; set; }
-        public string Sezona { get; set; }
-        public string Pobjednik { get; set; }
+        public string Naziv { get; set; } = "";
+        public string Vrsta { get; set; } = "";
+        public string Drzava { get; set; } = "";
+        public string Sezona { get; set; } = "";
+        public string Pobjednik { get; set; } = "";
         [Column("najbolji_igrac")] // mogućnost mapiranja
         public string? NajboljiIgrac { get; set; }
 
