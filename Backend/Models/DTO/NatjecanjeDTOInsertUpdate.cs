cd6d45e432edd9d1be852e37fade2f435d45fe43
Martin Galik <gale1508@gmail.com>
2025-02-28 20:10:33 +0100

    Mapiranje sređivanje

--- Backend/Models/DTO/NatjecanjeDTOInsertUpdate.cs
+++ Backend/Models/DTO/NatjecanjeDTOInsertUpdate.cs
@@ -3,11 +3,13 @@
 namespace Beckend.Models.DTO
 {
     public record NatjecanjeDTOInsertUpdate(
-        [Required(ErrorMessage = "Naziv obavezno")]
+        [Required(ErrorMessage = "Naziv je obavezan")]
         string Naziv,
         string Vrsta,
         string Drzava,
+        [Required(ErrorMessage = "Sezona mora biti upisana")]
         string Sezona,
+        [Required(ErrorMessage = "Pobjednik je obavezan")]
         string Pobjednik,
         string? NajboljiIgrac
         );
