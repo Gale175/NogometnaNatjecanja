75ce24043b822623d02d3b0b9d5c4bf5e6ba1dee
Martin Galik <gale1508@gmail.com>
2025-03-26 20:04:50 +0100

    Dodavanje entiteta Sifra u NatjecanjeDTORead

--- Backend/Models/DTO/NatjecanjeDTORead.cs
+++ Backend/Models/DTO/NatjecanjeDTORead.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// DTO za čitanje podataka o natjecanju.
     /// </summary>
+    /// /// <param name="Sifra">Jedinstvena šifra natjecanja.</param>
     /// <param name="Naziv">Naziv natjecanja.</param>
     /// <param name="Vrsta">Vrsta natjecanja- kup ili liga</param>
     /// <param name="Drzava">Država u kojoj se odigrava natjecanje</param>
